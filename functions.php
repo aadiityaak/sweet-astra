741fe8d22c2dd737cee0b06549bbeafca60dc598
aadiityaak <42263468+aadiityaak@users.noreply.github.com>
2021-12-06 21:51:25 +0700

    Add bootstrap

--- functions.php
+++ functions.php
@@ -19,6 +19,11 @@ define( 'CHILD_THEME_SWEET_ASTRA_VERSION', '1.0.0' );
 function child_enqueue_styles() {
 
 	wp_enqueue_style( 'sweet-astra-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_SWEET_ASTRA_VERSION, 'all' );
+	wp_enqueue_style( 'sweet-sweet-css', get_stylesheet_directory_uri() . '/assets/css/sweet.css', array('sweet-astra-theme-css'), CHILD_THEME_SWEET_ASTRA_VERSION, 'all' );
+	wp_enqueue_style( 'bootstrap-css', get_stylesheet_directory_uri() . '/assets/css/bootstrap.min.css', array(), CHILD_THEME_SWEET_ASTRA_VERSION, 'all' );
+
+	wp_enqueue_script( 'bootstrap-js', get_stylesheet_directory_uri() . '/assets/js/bootstrap.min.js', array('jquery'), CHILD_THEME_SWEET_ASTRA_VERSION, true );
+	wp_enqueue_script( 'sweet-sweet-js', get_stylesheet_directory_uri() . '/assets/js/sweet.js', array('jquery'), CHILD_THEME_SWEET_ASTRA_VERSION, true );
 
 }
 
